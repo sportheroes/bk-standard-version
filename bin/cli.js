4c305cb6d548d96c03b8aeb301c7ec1cbd13b32d
Olivier Lardenois <olardenois@gmail.com>
2024-11-21 09:46:44 +0100

    🔄 [MOD] Update node version to 18

--- bin/cli.js
+++ bin/cli.js
@@ -3,8 +3,8 @@ var standardVersion = require('../index')
 var cmdParser = require('../command')
 
 /* istanbul ignore if */
-if (process.version.match(/v(\d+)\./)[1] < 4) {
-  console.error('standard-version: Node v4 or greater is required. `standard-version` did not run.')
+if (process.version.match(/v(\d+)\./)[1] < 18) {
+  console.error('standard-version: Node v18 or greater is required. `standard-version` did not run.')
 } else {
   standardVersion(cmdParser.argv)
     .catch(() => {
